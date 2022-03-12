74d60a3a3f0dcdee872d61773c39da71222b9c0b
Nitharshni <63719707+codeadmin12@users.noreply.github.com>
2022-03-12 23:37:57 +1100

    Create listeriaScraper.py

---
+++ Phase_1/API_SourceCode/listeriaScraper.py
@@ -0,0 +1,186 @@
+# INSTALL BEAUTIFULSOUP
+# pip install beautifulsoup4
+
+from functools import partialmethod
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+from datetime import datetime
+
+#check if url exists in json list
+def check_url(url, list):
+    for article in list:
+        if (article["url"] == url):
+            return 1
+    return 0
+
+
+def listeria_scraper():
+    #base of the cdc url
+    base = "https://www.cdc.gov"
+
+    #go to the first listeria page
+    page = requests.get("https://www.cdc.gov/listeria/outbreaks/packaged-salad-mix-12-21/index.html")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    #load the disease and syndrome lists
+    diseaseFile = open('diseaseList.json')
+    diseaseList = json.load(diseaseFile)
+
+    syndromeFile = open('syndromeList.json')
+    syndromeList = json.load(syndromeFile)
+
+    #regex pattern for the index files and files with case numbers
+    patternIndex = re.compile("/listeria/outbreaks/.+/index.html")
+    patternLocation = re.compile("/listeria/outbreaks/.+/map.html")
+
+    #intialise data structures
+    articlesData =[]
+
+    #first find all the links in the base page
+    for link in soup.find_all('a'):
+
+        #initialise data structures
+        objects = { "diseases": [""], "syndromes": [""], "event_date": "", "locations": [""], }
+        data = { "url": "", "date_of_publication": "", "headline": "", "main_text": "", "reports": [objects] }
+        locations = []
+
+        #get different outbreak links
+        if (patternIndex.match(str(link.get('href')))):
+            route = str(link.get('href'))
+
+            #now navigate to individual outbreak pages
+            p = requests.get(base + route)
+            s = BeautifulSoup(p.content, 'html.parser')
+
+            #check current url is not already added to articlesData
+            if (check_url((base + str(link.get('href'))), articlesData)):
+                continue
+
+            #add url to articlesData
+            data["url"] = base + str(link.get('href'))
+
+            #find the date of publication
+            for para in s.find_all('p'):
+                if (str(para)).startswith("<p>Posted"):
+                    d = str(para)
+
+                    #remove excess tags and junk
+                    d = d.replace("<p>Posted ", "")
+                    d = d.replace("<p>Posted ", "")
+                    d = d.replace("</p>", "")
+                    d = d.replace("at ", "")
+                    d = d.replace("ET", "")
+                    d = d.strip()
+                    try:
+                        #convert data type from 'march 8, 2022'
+                        newDate = datetime.strptime(d,"%B %d, %Y")
+
+                        #add the date of publication
+                        data["date_of_publication"] = str(newDate.strftime('%Y-%m-%d xx:xx:xx'))
+                    except ValueError:
+                        try:
+                            #convert data type from 'June 10, 2015 10:30 AM'
+                            newDate = datetime.strptime(d,"%B %d, %Y %I:%M %p")
+                            data["date_of_publication"] = str(newDate.strftime('%Y-%m-%d %H:%M:xx'))
+                        except ValueError:
+                            #no match; set to empty string
+                            #year is mandatory?
+                            data["date_of_publication"] = 'xx-xx-xx xx:xx:xx'
+
+                #check if any other diseases are mentioned in the page
+                #add listeriosis as default
+                objects["diseases"].append("listeriosis")
+                for disease in diseaseList:
+                    if ((str(para)).find(disease["name"]) != -1) and (disease["name"] not in objects["diseases"]):
+                        objects["diseases"].append(disease["name"])
+
+                #check if any syndromes are mentioned in the page
+                for syndrome in syndromeList:
+                    if (str(para)).find(syndrome["name"]) != -1 and (syndrome["name"] not in objects["syndromes"]):
+                        objects["syndromes"].append(syndrome["name"])
+
+
+            #find the headline
+            #remove tags and junk
+            headline = str(s.title)
+            headline = headline.replace("<title>", "")
+            headline = headline.replace("</title>", "")
+            try:
+                headlines = headline.split('|')
+                data['headline'] = headlines[0]
+            except:
+                None
+                data['headline'] = headline
+
+
+            ##############   finding the main text
+
+
+
+
+            ################## finding the event date
+
+
+
+
+        #find the locations
+        if (patternLocation.match(str(link.get('href')))):
+            route = str(link.get('href'))
+            #print(base+route)
+
+            #now navigate to individual pages that end in map.html
+            p = requests.get(base + route)
+            s = BeautifulSoup(p.content, 'html.parser')
+
+            for t in s.find_all('td'):
+                patternState = re.compile("^(<td|<td>)[^State][^Case Count][^0-9]+</td>")
+                if (patternState.match(str(t))):
+                    state = str(t)
+                    state = state.replace("<td>", "")
+                    state = state.replace("</td>", "")
+                    state = state.replace("State", "")
+                    state = state.replace("<strong>Total</strong>", "")
+                    state = state.replace("<p>", "")
+                    state = state.replace("</p>", "")
+                    state = state.replace('<td class="row">', "")
+                    state = state.replace("<strong>Total ill persons</strong>", "")
+                    state = state.replace("\n", "")
+                    if state not in locations and state != "":
+                        locations.append(state)
+        
+        
+           #if locations is empty then 
+           #DONT KNOW HOW TO PARSE COLLAPSABLE TABLES :(
+            #if (locations == []):
+                #print("empt")
+            #print(locations)
+
+
+
+
+            objects["locations"] = locations
+            data["reports"] = objects
+            articlesData.append(data)
+
+    return articlesData
+
+#takes a while to print
+if __name__ == "__main__":
+    for i in listeria_scraper():
+        print(i)
+        print("\n")
+
+'''
+url - done
+date of pub- done
+headline - done
+main_text -
+
+reports:
+    diseases - done
+    syndromes - done
+    event_date
+    locations
+'''
