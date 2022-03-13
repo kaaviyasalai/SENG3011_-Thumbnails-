19dd09354d1f969ae18f524bdb210cbd74e047a4
ericphungg1 <80400120+ericphungg1@users.noreply.github.com>
2022-03-14 01:21:28 +1100

    Create api.py

---
+++ Phase_1/API_SourceCode/api.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+es = Elasticsearch('10.0.1.10', port=9200)
+
+
+@app.route('/')
+def home():
+    return render_template('search.html')
+
+@app.route('/search/results', methods=['GET', 'POST'])
+def search_request():
+    search_term = request.form["input"]
+    res = es.search(
+        index="scrape-sysadmins", 
+        size=20, 
+        #database fields
+    )
+    return render_template('results.html', res=res )
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(host='0.0.0.0', port=5000)
