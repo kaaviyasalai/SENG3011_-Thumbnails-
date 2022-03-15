0552e32b349e6780eebc6bef727b8e7906b61372
ericphungg1 <80400120+ericphungg1@users.noreply.github.com>
2022-03-15 12:29:59 +1100

    Update api.py

--- Phase_1/API_SourceCode/api.py
+++ Phase_1/API_SourceCode/api.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template, request
-from elasticsearch import Elasticsearch
-
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pymongo
+  
+connection_url = 'mongodb+srv://thumbnails:thumbnails.@cluster0.lfkm3.mongodb.net/SENG3011?retryWrites=true&w=majority'
 app = Flask(__name__)
-es = Elasticsearch('10.0.1.10', port=9200)
+client = pymongo.MongoClient(connection_url)
 
+Database = client.get_database('SENG3011')
 
-@app.route('/')
-def home():
-    return render_template('search.html')
+Table = pymongo.collection.Collection(Database, 'SENG3011_collection')
 
-@app.route('/search/results', methods=['GET', 'POST'])
-def search_request():
-    search_term = request.form["input"]
-    res = es.search(
-        index="scrape-sysadmins", 
-        size=20, 
-        #database fields
-    )
-    return render_template('results.html', res=res )
+@app.route('/find', methods=['GET'])
+def find():
+    query = Table.find()
+    output = {}
+    i = 0 
+    for x in query:
+        output[i] = x
+        output[i].pop('_id')
+        i+=1
+    return jsonify(output)
+
+@app.route('/find-one/<argument>/<value>/', methods=['GET'])
+def findOne(argument, value):
+    queryObject = {argument: value}
+    query = Table.find_one(queryObject)
+    query.pop('_id')
+    return jsonify(query)
 
 if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port=8000)
+
